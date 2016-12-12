53ffcd6b317ff117fd9e1ff411696ca83638a80a
dangerstephen <dangersteve1@gmail.com>
2016-12-12 15:17:58 -0800

    finished the logic now will make conf_app

--- 3_talks_data.rb
+++ 3_talks_data.rb
@@ -3,11 +3,22 @@ require 'ffaker'
 puts "Speakers"
 puts "========"
 5.times do
-  p {} # a speaker data object
+  speaker = {
+              first_name: FFaker::Name.first_name,
+              last_name: FFaker::Name.last_name,
+              email: FFaker::Internet.free_email,
+            }
+  puts speaker
 end
+
+
 
 puts "\nTalks"
 puts "====="
 5.times do
-  p {} # a talk data object
+  talk = {
+    topic: FFaker::Movie.title,
+    duration: Random.new.rand(30..90)
+  }
+  puts talk
 end
